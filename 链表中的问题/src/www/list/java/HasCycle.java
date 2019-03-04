0b6d074d9fb59143cec8e3dbbf056bf0b5ef4dbe
excellent01 <2418512129@qq.com>
2019-03-04 16:32:39 +0800

    是否带环

---
+++ 链表中的问题/src/www/list/java/HasCycle.java
@@ -0,0 +1,24 @@
+package www.list.java;
+
+/**
+ *判断链表是否带环，快慢指针法。
+ */
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while( fast != null && fast.next != null){
+            if(fast == slow){
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+
+}
+
