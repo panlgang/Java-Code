052a3dd7cd4a0aacc09e4a95d8cf3bb9a7d5681a
excellent01 <2418512129@qq.com>
2019-04-26 22:10:55 +0800

    体验StackOverflow错误

---
+++ TestJVM/src/com/github/excellent/TestStackOverFlow.java
@@ -0,0 +1,12 @@
+package com.github.excellent;
+
+/**
+ * 递归调用即可
+ * @auther plg
+ * @date 2019/4/26 22:08
+ */
+public class TestStackOverFlow {
+    public static void main(String[] args) {
+        main(args);
+    }
+}
