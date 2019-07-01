920533725df06b5d8936c6719f52b1a24688497f
excellent01 <2418512129@qq.com>
2019-07-01 18:54:27 +0800

    重定向

---
+++ web01/src/main/java/RedirectServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class RedirectServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request);
+        // 重定向简写方式,页面地址发生改变
+        response.sendRedirect("/Form.html");
+        System.out.println(request);
+
+    }
+}
